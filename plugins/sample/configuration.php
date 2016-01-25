8274a7ad2daec41f1a25880882d1ca254bada2e1
Sumo Vander <dgadoury+catberg@sumologic.com>
2016-01-25 12:08:45 -0500

    initial commit

---
+++ plugins/sample/configuration.php
@@ -0,0 +1,50 @@
+<?php
+/*************************
+  Coppermine Photo Gallery
+  ************************
+  Copyright (c) 2003-2016 Coppermine Dev Team
+  v1.0 originally written by Gregory Demar
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License version 3
+  as published by the Free Software Foundation.
+
+  ********************************************
+  Coppermine version: 1.6.01
+  $HeadURL$
+  $Revision$
+**********************************************/
+
+if (!defined('IN_COPPERMINE')) die('Not in Coppermine...');
+
+$name = $lang_plugin_sample['config_name'];
+$description = $lang_plugin_sample['config_description'];
+$author = 'Coppermine Development Team';
+$version = '1.7';
+$plugin_cpg_version = array('min' => '1.6');
+/*
+ * $extra_info is displayed with the title of a plugin that IS installed and
+ * can be used to present extra information.  In this case we show a complex
+ * example that forms a button the user can click.
+ */
+$extra_info = <<<EOT
+    <table border="0" cellspacing="0" cellpadding="0">
+    <tr>
+        <td><a href="docs/en/plugins.htm" title="{$lang_plugin_sample['plugin_documentation']}" class="admin_menu">{$lang_plugin_sample['plugin_documentation']}</a></td>
+    </tr>
+    </table>
+EOT;
+/*
+ * $install_info is displayed with the title of a plugin that is NOT installed and
+ * can be used to present extra information. In this case we show a complex
+ * example that forms a button the user can click.
+ */
+$install_info=<<<EOT
+    <table border="0" cellspacing="0" cellpadding="0">
+    <tr>
+        <td><a href="http://coppermine-gallery.net/forum/index.php?board=97.0" title="{$lang_plugin_sample['plugin_support']}" class="admin_menu">{$lang_plugin_sample['plugin_support']}</a></td>
+    </tr>
+    </table>
+EOT;
+
+?>
---
+++ sql/update.sql
@@ -0,0 +1,40 @@
+##  ********************************************
+##  Coppermine Photo Gallery
+##  ************************
+##  Copyright (c) 2003-2016 Coppermine Dev Team
+##  v1.0 originally written by Gregory Demar
+##
+##  This program is free software; you can redistribute it and/or modify
+##  it under the terms of the GNU General Public License version 3
+##  as published by the Free Software Foundation.
+##
+##  ********************************************
+##  Coppermine version: 1.6.01
+##  $HeadURL$
+##  $Revision$
+##  ********************************************
+
+# The following line has to be removed when the moderator group feature will be re-enabled!
+UPDATE CPG_albums SET moderator_group = 0;
+
+INSERT INTO CPG_config VALUES ('upload_create_album_directory', '1');
+INSERT INTO CPG_config VALUES ('ecard_captcha', '1');
+INSERT INTO CPG_config VALUES ('cookies_need_consent', '0');
+INSERT INTO CPG_config VALUES ('album_sort_order', 'pa');
+INSERT INTO CPG_config VALUES ('custom_sortorder_thumbs', '1');
+INSERT INTO CPG_config VALUES ('link_last_upload', '0');
+INSERT INTO CPG_config VALUES ('editpics_ignore_newer_than', '0');
+
+ALTER TABLE CPG_users ADD user_password_salt varchar(255) NOT NULL default '';
+ALTER TABLE CPG_users ADD user_password_hash_algorithm varchar(25) NOT NULL default '';
+ALTER TABLE CPG_users ADD user_password_iterations varchar(25) NOT NULL default '';
+
+ALTER TABLE CPG_comments CHANGE `msg_date` `msg_date` datetime NOT NULL default '1000-01-01 00:00:00';
+ALTER TABLE CPG_pictures CHANGE `mtime` `mtime` datetime NOT NULL default '1000-01-01 00:00:00';
+ALTER TABLE CPG_users CHANGE `user_lastvisit` `user_lastvisit` datetime NOT NULL default '1000-01-01 00:00:00';
+ALTER TABLE CPG_users CHANGE `user_regdate` `user_regdate` datetime NOT NULL default '1000-01-01 00:00:00';
+
+INSERT INTO CPG_config VALUES ('batch_add_hide_existing_files', '0');
+INSERT INTO CPG_config VALUES ('only_empty_albums', '0');
+INSERT INTO CPG_config VALUES ('user_manager_hide_file_stats', '0');
+INSERT INTO CPG_config VALUES ('album_uploads_default', 'NO');
